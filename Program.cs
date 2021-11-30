63ad729d341794e41ac4d5d9e314fceed2bd4c4e
shadowsental <mattias.celius@gmail.com>
2021-11-30 10:26:22 +0100

    Pretty cool text

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace CsharpTestMattias
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Pretty cool, ain't it?");
         }
     }
 }
